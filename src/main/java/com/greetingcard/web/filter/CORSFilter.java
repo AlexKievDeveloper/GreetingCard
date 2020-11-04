c9545983b908135c3c3d9fb40d09b8eec2269ac5
RomanKl <klisch.roma@gmail.com>
2020-11-04 18:23:09 +0200

    Added CORSFilter - added CORSFilter; - added tests

---
+++ src/main/java/com/greetingcard/web/filter/CORSFilter.java
@@ -0,0 +1,38 @@
+package com.greetingcard.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        chain.doFilter(request, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/test/java/com/greetingcard/web/filter/СORSFilterTest.java
@@ -0,0 +1,44 @@
+package com.greetingcard.web.filter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class СORSFilterTest {
+
+    @InjectMocks
+    CORSFilter corsFilter;
+    @Mock
+    private HttpServletRequest servletRequest;
+    @Mock
+    private HttpServletResponse servletResponse;
+    @Mock
+    private FilterChain filterChain;
+
+    @Test
+    void doFilter() throws IOException, ServletException {
+        when(servletRequest.getMethod()).thenReturn("OPTIONS");
+
+        corsFilter.doFilter(servletRequest,servletResponse,filterChain);
+
+        verify(servletResponse).addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        verify(servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+        verify(servletResponse).setStatus(HttpServletResponse.SC_ACCEPTED);
+    }
+}
