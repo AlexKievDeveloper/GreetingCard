a65e72a6a1b219baf3b9c472f355f646f5500f9e
AlexKievDeveloper <glushkov18@gmail.com>
2020-12-10 16:29:03 +0200

    Refactoring tests after adding file storage to s3 bucket
    1. Refactoring tests related to files saving in JdbcCongratulationDaoITest, DefaultCongratulationServiceITest, DefaultCongratulationServiceSystemTest.
    2. Refactoring others tests in DefaultSecurityServiceSystemTest, UserControllerSystemTest.
    3. Pom.xml added dependensy commons.io.

--- src/test/java/com/greetingcard/web/controller/UserControllerSystemTest.java
+++ src/test/java/com/greetingcard/web/controller/UserControllerSystemTest.java
@@ -26,9 +26,6 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -187,8 +184,10 @@ class UserControllerSystemTest {
     @DisplayName("Request to change password ")
     @ExpectedDataSet("usersAfterChangePassword.xml")
     void testChangePassword() throws Exception {
-
         //prepare
+        User user = User.builder()
+                .login("user")
+                .build();
         Map<String, String> userCredential = new HashMap<>();
         userCredential.put("login", "user");
         userCredential.put("oldPassword", "user");
@@ -198,6 +197,7 @@ class UserControllerSystemTest {
 
         //when
         mockMvc.perform(put("/api/v1/user/password")
+                .sessionAttr("user", user)
                 .contentType(APPLICATION_JSON_VALUE)
                 .accept(APPLICATION_JSON_VALUE)
                 .content(json))
